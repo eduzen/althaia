5d5348e685a5a2e05a95a73ec3d4cda93936b28f
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2023-10-15 22:34:21 +0200

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.12-slim
+FROM python:3.12-slim as base
 
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
@@ -19,3 +19,15 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 CMD [ "python", "./manage.py", "runserver", "0.0.0.0:8000" ]
+
+FROM base as prod
+
+RUN python manage.py collectstatic --noinput
+
+CMD [ "gunicorn", "althaia.wsgi:application", \
+  "--bind", "0.0.0.0:8000", \
+  "--workers", "1", \
+  "--threads", "3", \
+  "--access-logfile", "-",\
+  "--error-logfile", "-" \
+]
--- althaia/settings.py
+++ althaia/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -123,3 +124,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
